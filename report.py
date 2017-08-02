5a56e0b6897b12a05a39485fe775dfd09b7ec12d
Leon-Mac <leonardoli525@gmail.com>
2017-08-02 18:56:21 -0400

    Lint for pep8

--- report.py
+++ report.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 import psycopg2
+DB_name = "news"
+
 
 def db_connect():
     try:
-        conn = psycopg2.connect("dbname=news")
+        conn = psycopg2.connect("dbname=" + DB_name)
     except psycopg2.DatabaseError as e:
-        print('Database error, please refer to the README to set up the databse')
+        print('Database error, please refer to the README' +
+              ' to set up the databse')
         print(e)
         exit()
     return (conn, conn.cursor())
+
 
 def execute_query(query):
     cur.execute(query)
     result = cur.fetchall()
     return result
+
 
 def print_top_articles():
     query = """SELECT articles.title, count(*) AS num FROM articles, log
@@ -26,6 +31,7 @@ def print_top_articles():
     for row in result:
         print('\t"{0}" -- {1} views'.format(row[0], row[1]))
 
+
 def print_top_authors():
     query = """SELECT authors.name, count(*) AS num FROM authors
                 LEFT JOIN articles ON authors.id=articles.author
@@ -36,6 +42,7 @@ def print_top_authors():
     print('Most popular article authors of all time:')
     for row in result:
         print('\t{0} -- {1} views'.format(row[0], row[1]))
+
 
 def print_errors_over_one():
     query = """SELECT d1,100*cast(err AS float)/cast(total AS float) AS percent
@@ -50,9 +57,11 @@ def print_errors_over_one():
     for row in result:
         print('\t{0:%B %d, %Y} -- {1:.2f}%'.format(row[0], row[1]))
 
+
 def close_db():
     cur.close()
     conn.close()
+
 
 def main():
     print_top_articles()
