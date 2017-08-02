d58e13a0cf83ef94d7c4e11e9f13e80ae8215b56
Leon-Mac <leonardoli525@gmail.com>
2017-08-02 18:41:44 -0400

    Refactor the code per feedback 	* change the structure for modulize 	* change the SQL for faster processing

--- report.py
+++ report.py
@@ -1,43 +1,65 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import psycopg2
 
-conn = psycopg2.connect("dbname=news")
+def db_connect():
+    try:
+        conn = psycopg2.connect("dbname=news")
+    except psycopg2.DatabaseError as e:
+        print('Database error, please refer to the README to set up the databse')
+        print(e)
+        exit()
+    return (conn, conn.cursor())
 
-cur = conn.cursor()
+def execute_query(query, cur):
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
 
-query1 = "SELECT articles.title, count(*) AS num FROM articles, log " \
-    "WHERE log.path LIKE concat('%', articles.slug) GROUP BY " \
-    "articles.title ORDER BY num DESC"
-cur.execute(query1)
-result = cur.fetchall()
+def print_top_articles(cur):
+    query = """SELECT articles.title, count(*) AS num FROM articles, log
+                WHERE log.path = '/article/' || articles.slug
+                GROUP BY articles.title ORDER BY num DESC LIMIT 3"""
 
-print('Most popular three articles of all time:')
-for row in result[:3]:
-    print('\t"' + row[0] + '" -- ' + str(row[1]) + ' views')
+    result = execute_query(query, cur)
 
-query2 = "SELECT authors.name, count(*) AS num FROM authors " \
-    "LEFT JOIN articles ON authors.id=articles.author " \
-    "LEFT JOIN log ON log.path LIKE concat('%', articles.slug) " \
-    "GROUP BY authors.name ORDER BY num DESC"
-cur.execute(query2)
-result = cur.fetchall()
+    print('Most popular three articles of all time:')
+    for row in result:
+        print('\t"{0}" -- {1} views'.format(row[0], row[1]))
 
-print('Most popular article authors of all time:')
-for row in result:
-    print('\t' + row[0] + ' -- ' + str(row[1]) + ' views')
+def print_top_authors(cur):
+    query = """SELECT authors.name, count(*) AS num FROM authors
+                LEFT JOIN articles ON authors.id=articles.author
+                LEFT JOIN log ON log.path = '/article/' || articles.slug
+                GROUP BY authors.name ORDER BY num DESC"""
+    result = execute_query(query, cur)
 
+    print('Most popular article authors of all time:')
+    for row in result:
+        print('\t{0} -- {1} views'.format(row[0], row[1]))
 
-query3 = "SELECT d1,100*cast(err AS float)/cast(total AS float) AS percent " \
-    "FROM (SELECT time::date AS d1, count(*) AS err " \
-    "FROM log WHERE status LIKE'%404%' GROUP BY d1) AS first " \
-    "LEFT JOIN (SELECT time::date AS d2, count(*) AS total " \
-    "FROM log GROUP BY d2) AS second ON d1=d2"
-cur.execute(query3)
-result = cur.fetchall()
+def print_errors_over_one(cur):
+    query = """SELECT d1,100*cast(err AS float)/cast(total AS float) AS percent
+                FROM (SELECT time::date AS d1, count(*) AS err
+                FROM log WHERE status LIKE'%404%' GROUP BY d1) AS first
+                LEFT JOIN (SELECT time::date AS d2, count(*) AS total
+                FROM log GROUP BY d2) AS second ON d1=d2
+                WHERE 100*cast(err AS float)/cast(total AS float) > 1"""
+    result = execute_query(query, cur)
 
-print('On which days did more than 1% of requests lead to errors:')
-for row in result:
-    if (row[1] >= 1):
-        print('\t' + str(row[0]) + ' -- ' + '{0:.2f}'.format(row[1]) + '%')
-cur.close()
-conn.close()
+    print('On which days did more than 1% of requests lead to errors:')
+    for row in result:
+        print('\t{0:%B %d, %Y} -- {1:.2f}%'.format(row[0], row[1]))
+
+def close_db(conn, cur):
+    cur.close()
+    conn.close()
+
+def main():
+    (conn, cur) = db_connect()
+    print_top_articles(cur)
+    print_top_authors(cur)
+    print_errors_over_one(cur)
+    close_db(cur, conn)
+
+if __name__ == '__main__':
+    main()
